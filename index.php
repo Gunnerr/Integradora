5a6115f0a7b6a9f21a2577b26fcadf84c365eb99
Gunner <furia2010598@hotmail.com>
2017-06-15 13:51:01 -0500

    Icono
    Se agrgo el icono

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <link href="imagen/logo.png" rel="shortcut icon" />
         <meta charset="UTF-8">
         <title>COBACH PLANTEL 16</title>
     </head>
